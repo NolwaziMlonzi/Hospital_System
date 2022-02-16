8eddbaa00fad3d7f4ec6aa0761905dc4e3dc13c5
Nolwazi <root96void@gmail.com>
2022-02-16 19:53:01 +0200

    new commit

--- Hospital_System/App_Start/BundleConfig.cs
+++ Hospital_System/App_Start/BundleConfig.cs
@@ -29,9 +29,10 @@ namespace Hospital_System
                      "~/lib/font-awesome/css/all.css"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                     "~/Content/bootstrap.css"
-                //"~/Content/bootstrap_ocean.css"
-                     // "~/Content/site.css"
+                 "~/Content/bootstrap_slate.css"
+                      // "~/Content/bootstrap.css"
+                      //"~/Content/bootstrap_ocean.css"
+                      // "~/Content/site.css"
                       ));
             //Bundle for app methods.
             bundles.Add(new ScriptBundle("~/bundles/methods").Include(
